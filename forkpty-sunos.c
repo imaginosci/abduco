imaginosci/abduco

commit 8c81939b9df4465fc18c62ba3c8772d9641c01c3

Fallback to TTYNAME_MAX if TTY_NAME_MAX is not defined

forkpty-sunos.c
@@ -22,6 +22,10 @@
 #include <stropts.h>
 #include <unistd.h>
 
+#ifndef TTY_NAME_MAX
+#define TTY_NAME_MAX TTYNAME_MAX
+#endif
+
 pid_t forkpty(int *master, char *name, struct termios *tio, struct winsize *ws)
 {
 	int	slave;

commit 912d3686aa480db3567e6b1d05cc4d7be4cecf82

forkpty-sunos: Avoid relying on TTYNAME_MAX
Solaris 11.4 provides TTY_NAME_MAX, but not all environments expose
TTYNAME_MAX. The fallback currently assumes TTYNAME_MAX is always
available, which breaks the local forkpty implementation before abduco
can build on Solaris.

Include the limits definitions explicitly and keep a small constant
fallback for systems that provide neither name.

Fixes: 8c81939 ("Fallback to TTYNAME_MAX if TTY_NAME_MAX is not defined")
Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

forkpty-sunos.c
@@ -18,6 +18,7 @@
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <stdlib.h>
 #include <strings.h>
 #include <stropts.h>
@@ -26,7 +27,11 @@
 #include "abduco.h"
 
 #ifndef TTY_NAME_MAX
-#define TTY_NAME_MAX TTYNAME_MAX
+# ifdef TTYNAME_MAX
+#  define TTY_NAME_MAX TTYNAME_MAX
+# else
+#  define TTY_NAME_MAX 128
+# endif
 #endif
 
 pid_t forkpty(int *master, char *name, struct termios *tio, struct winsize *ws)